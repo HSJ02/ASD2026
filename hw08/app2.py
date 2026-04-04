daa8eae1b514746dc470a36c7d953fa3426d3957
SeungJae Hwang <117615107+HSJ02@users.noreply.github.com>
2026-04-04 20:47:28 +0900

    Add files via upload

---
+++ hw08/app2.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/blog_list")
+def blog():
+    return render_template("blog_list.html")
+
+@app.route("/about")
+def about():
+    return render_template("about_me.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
